518852df0ae7a75a716dac08c3d4070f131b0463
Fafinho0903 <fabriciocds090310@gmail.com>
2025-10-17 16:44:27 -0300

    Add files via upload

---
+++ QUESTAO-7/QUESTAO-7/Program.cs
@@ -0,0 +1,32 @@
+﻿int tab, taba, tabb, i, m;
+
+
+Console.WriteLine("Escreva o 1° número para ser multiplicado:");
+tab = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o 2° número para ser multiplicado:");
+taba = int.Parse(Console.ReadLine());
+Console.WriteLine("\r");
+
+i = 1;
+tabb = tab;
+m = 1;
+while (tabb < taba+1)
+{
+    Console.WriteLine($"{tabb} * {m} = {tabb * m}\n");
+
+    if (i != 11 )
+    {
+        i = i + 1;
+        m = m + 1;
+
+    }
+ 
+        if (m == 11)
+        {
+            i = 1;
+            m = 1;
+            tabb = tabb + 1;
+        Console.WriteLine("\r");
+        }
+}
+
